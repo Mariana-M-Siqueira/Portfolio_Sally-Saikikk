dbbea7c45bd111211430caaab00de1b0983d7f73
Mariana Matias Siqueira <mariana.siqueira28900@gmail.com>
2024-05-21 17:13:12 -0300

    modal exibindo imagem

--- script/modal__exibindo.js
+++ script/modal__exibindo.js
@@ -1,11 +1,9 @@
-const modal = document.querySelector('.principal__modal__exibindo');
+const modal = document.querySelector('.principal__modal');
 const imagens = document.querySelectorAll('.principal__galeria img');
-const fecharModal = document.querySelector('.modal__exibindo-fechar');
+const fecharModal = document.querySelector('.modal-fechar');
 const areaImg = document.querySelector('.modal__exibindo-img');
 const areaTitulo = document.querySelector('.item-titulo p');
 const areaCategoria = document.querySelector('.categoria p');
-
-modal.close();
 
 imagens.forEach((imagem) => {
     let imgSelecionada = imagem.src;
@@ -18,12 +16,11 @@ imagens.forEach((imagem) => {
         areaImg.setAttribute('src',`${imgSelecionada}`);
 
         imgCategoria.forEach((categoria) => {
-            areaCategoria.textContent += categoria;
+            areaCategoria.textContent = categoria;
         })
 
         areaTitulo.textContent = imgTitulo;
     })
-    console.log(imgCategoria)
 })
 
 fecharModal.addEventListener('click', () => {
