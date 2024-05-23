9ce9549c60c516860dc2ee4d666ab2ab46fed901
Mariana Matias Siqueira <mariana.siqueira28900@gmail.com>
2024-05-23 14:57:37 -0300

    estilização style.css e header_index.css finalizada

--- script/modal__exibindo.js
+++ script/modal__exibindo.js
@@ -1,29 +1,29 @@
-const modal = document.querySelector('.principal__modal');
-const imagens = document.querySelectorAll('.principal__galeria img');
-const fecharModal = document.querySelector('.modal-fechar');
-const areaImg = document.querySelector('.modal__exibindo-img');
-const areaTitulo = document.querySelector('.item-titulo p');
-const areaCategoria = document.querySelector('.categoria p');
+// const modal = document.querySelector('.principal__modal');
+// const imagens = document.querySelectorAll('.principal__galeria img');
+// const fecharModal = document.querySelector('.modal-fechar');
+// const areaImg = document.querySelector('.modal__exibindo-img');
+// const areaTitulo = document.querySelector('.item-titulo p');
+// const areaCategoria = document.querySelector('.categoria p');
 
-imagens.forEach((imagem) => {
-    let imgSelecionada = imagem.src;
-    let imgTitulo = imagem.title;
-    let imgCategoria = imagem.classList;
+// imagens.forEach((imagem) => {
+//     let imgSelecionada = imagem.src;
+//     let imgTitulo = imagem.title;
+//     let imgCategoria = imagem.classList;
 
-    imagem.addEventListener('click', () => {
-        modal.showModal();
-        modal.classList.remove('close');
-        areaImg.setAttribute('src',`${imgSelecionada}`);
+//     imagem.addEventListener('click', () => {
+//         modal.showModal();
+//         modal.classList.remove('displayNone');
+//         areaImg.setAttribute('src',`${imgSelecionada}`);
 
-        imgCategoria.forEach((categoria) => {
-            areaCategoria.textContent = categoria;
-        })
+//         imgCategoria.forEach((categoria) => {
+//             areaCategoria.textContent = categoria;
+//         })
 
-        areaTitulo.textContent = imgTitulo;
-    })
-})
+//         areaTitulo.textContent = imgTitulo;
+//     })
+// })
 
-fecharModal.addEventListener('click', () => {
-    modal.close();
-    modal.classList.add('close');
-})
+// fecharModal.addEventListener('click', () => {
+//     modal.close();
+//     modal.classList.add('displayNone');
+// })
